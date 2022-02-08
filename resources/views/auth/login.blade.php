75527b8113ab8a3c0c1e865efbf09af3b683ecce
Lafllamme <lafllamme@MacBook-Pro-von-Lafllamme.local>
2022-02-08 18:37:33 +0100

    improves night mode

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,3 +1,8 @@
+<link rel="stylesheet" href="/css/app.css">
+<script defer src="/js/alpine.js"></script>
+<script defer src="/js/app.js"></script>
+
+
 <x-guest-layout>
     <x-auth-card>
         <x-slot name="logo">
@@ -26,10 +31,7 @@
             <div class="mt-4">
                 <x-label for="password" :value="__('Password')" />
 
-                <x-input id="password" class="block mt-1 w-full"
-                                type="password"
-                                name="password"
-                                required autocomplete="current-password" />
+                <x-input id="password" class="block mt-1 w-full" type="password" name="password" required autocomplete="current-password" />
             </div>
 
             <!-- Remember Me -->
@@ -42,9 +44,9 @@
 
             <div class="flex items-center justify-end mt-4">
                 @if (Route::has('password.request'))
-                    <a class="underline text-sm text-gray-600 hover:text-gray-900" href="{{ route('password.request') }}">
-                        {{ __('Forgot your password?') }}
-                    </a>
+                <a class="underline text-sm text-gray-600 hover:text-gray-900" href="{{ route('password.request') }}">
+                    {{ __('Forgot your password?') }}
+                </a>
                 @endif
 
                 <x-button class="ml-3">
